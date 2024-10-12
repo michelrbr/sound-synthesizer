00693bb1796b90964deec20a83b33937374db6b4
Michel Ribeiro <michelrbr@gmail.com>
2024-10-12 16:31:54 -0300

    Update WavetableSynthesizer exposing properties as StateSlow

--- app/src/main/java/br/com/michel/soundsynthesizer/domain/WavetableSynthesizer.kt
+++ app/src/main/java/br/com/michel/soundsynthesizer/domain/WavetableSynthesizer.kt
@@ -1,10 +1,16 @@
 package br.com.michel.soundsynthesizer.domain
 
+import kotlinx.coroutines.flow.StateFlow
+
 interface WavetableSynthesizer {
-    suspend fun play()
-    suspend fun stop()
-    suspend fun isPlaying(): Boolean
+    val selectedWavetable: StateFlow<Wavetable?>
+    val wavetables: StateFlow<List<Wavetable>>
+    val frequencyInHz: StateFlow<Float>
+    val volumeInDb: StateFlow<Float>
+    val isPlaying: StateFlow<Boolean>
     suspend fun setFrequency(frequencyInHz: Float)
     suspend fun setVolume(volumeInDb: Float)
     suspend fun setWavetable(wavetable: Wavetable)
+    suspend fun play()
+    suspend fun stop()
 }
