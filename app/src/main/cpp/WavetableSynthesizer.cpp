b926a87d452a51ce583cf752d5f7af90c1e8a27e
Michel Ribeiro <michelrbr@gmail.com>
2024-10-25 12:24:07 -0300

    Play and stop a simple sound

--- app/src/main/cpp/WavetableSynthesizer.cpp
+++ app/src/main/cpp/WavetableSynthesizer.cpp
@@ -1,16 +1,40 @@
 #include "Log.h"
 #include "Wavetable.h"
 #include "WavetableSynthesizer.h"
+#include <oboe/Oboe.h>
 
 namespace soundsynthesizer {
+
     void WavetableSynthesizer::play() {
         LOGD("synthesizer playing");
         _isPlaying = true;
+
+        std::lock_guard<std::mutex> lock(mLock);
+        oboe::AudioStreamBuilder builder;
+        oboe::Result result = builder.setSharingMode(oboe::SharingMode::Exclusive)
+                ->setPerformanceMode(oboe::PerformanceMode::LowLatency)
+                ->setChannelCount(channelCount)
+                ->setSampleRate(sampleRate)
+                ->setSampleRateConversionQuality(oboe::SampleRateConversionQuality::Medium)
+                ->setFormat(oboe::AudioFormat::Float)
+                ->setDataCallback(this)
+                ->openStream(mStream);
+
+        if (result != oboe::Result::OK) return;
+
+        mStream->requestStart();
     }
 
     void WavetableSynthesizer::stop() {
         LOGD("synthesizer stopped");
         _isPlaying = false;
+
+        std::lock_guard<std::mutex> lock(mLock);
+        if (mStream) {
+            mStream->stop();
+            mStream->close();
+            mStream.reset();
+        }
     }
 
     bool WavetableSynthesizer::isPlaying() const {
@@ -28,5 +52,22 @@ namespace soundsynthesizer {
 
     void WavetableSynthesizer::setWavetable(Wavetable wavetable) {
         LOGD("synthesizer wavetable selected: %.d", static_cast<int>(wavetable));
+    }
+
+    oboe::DataCallbackResult WavetableSynthesizer::onAudioReady(
+            oboe::AudioStream *oboeStream,
+            void *audioData,
+            int32_t numFrames) {
+        auto *floatData = (float *) audioData;
+
+        for (int i = 0; i < numFrames; ++i) {
+            float sampleValue = amplitude * sinf(mPhase);
+            for (int j = 0; j < channelCount; j++) {
+                floatData[i * channelCount + j] = sampleValue;
+            }
+            mPhase += mPhaseIncrement;
+            if (mPhase >= kTwoPi) mPhase -= kTwoPi;
+        }
+        return oboe::DataCallbackResult::Continue;
     }
 }
