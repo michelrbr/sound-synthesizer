457561680e6a19c1adae727bc153bb0f1cea75a2
Michel Ribeiro <michelrbr@gmail.com>
2025-03-24 16:23:28 -0300

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.michel.soundsynthesizer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.michel.soundsynthesizer"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
