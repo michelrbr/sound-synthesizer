c234f6f9a866ccc2a3f2cb55fb543ca2d23324c7
Michel Ribeiro <michelrbr@gmail.com>
2025-03-25 21:30:49 -0300

    Adapt WavetableSelection to landscape orientation

--- app/src/main/java/br/com/michel/soundsynthesizer/presentation/screen/component/WavetableSelection.kt
+++ app/src/main/java/br/com/michel/soundsynthesizer/presentation/screen/component/WavetableSelection.kt
@@ -1,5 +1,6 @@
 package br.com.michel.soundsynthesizer.presentation.screen.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import br.com.michel.soundsynthesizer.domain.Wavetable
 import br.com.michel.soundsynthesizer.presentation.screen.model.WavetableState
@@ -27,28 +29,57 @@ fun WavetableSelection(
     wavetables: List<WavetableState>,
     onSoundSelected: (Wavetable) -> Unit
 ) {
-    LazyVerticalGrid(
-        modifier = modifier,
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(AppTheme.spacing.small),
-        horizontalArrangement = Arrangement.spacedBy(AppTheme.spacing.small)
-    ) {
-        items(wavetables) { prop ->
-            Button(
-                onClick = { onSoundSelected(prop.wavetable) },
-                colors = ButtonDefaults
-                    .buttonColors(Color(prop.color), Color.White),
-                shape = RoundedCornerShape(AppTheme.spacing.small),
-                modifier = Modifier
-                    .size(100.dp)
-                    .padding(
-                        if (prop.isSelected) 0.dp else 5.dp
-                    )
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
+
+    when (LocalConfiguration.current.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> LazyVerticalGrid(
+            modifier = modifier,
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(AppTheme.spacing.small),
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.spacing.small)
+        ) {
+            items(wavetables) { prop ->
+                Button(
+                    onClick = { onSoundSelected(prop.wavetable) },
+                    colors = ButtonDefaults
+                        .buttonColors(Color(prop.color), Color.White),
+                    shape = RoundedCornerShape(AppTheme.spacing.small),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(
+                            if (prop.isSelected) 0.dp else 5.dp
+                        )
                 ) {
-                    Text(prop.name)
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(prop.name)
+                    }
+                }
+            }
+        }
+        else -> LazyVerticalGrid(
+            modifier = modifier,
+            columns = GridCells.Fixed(4),
+            verticalArrangement = Arrangement.spacedBy(AppTheme.spacing.small),
+            horizontalArrangement = Arrangement.spacedBy(AppTheme.spacing.small)
+        ) {
+            items(wavetables) { prop ->
+                Button(
+                    onClick = { onSoundSelected(prop.wavetable) },
+                    colors = ButtonDefaults
+                        .buttonColors(Color(prop.color), Color.White),
+                    shape = RoundedCornerShape(AppTheme.spacing.small),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(
+                            if (prop.isSelected) 0.dp else 5.dp
+                        )
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(prop.name)
+                    }
                 }
             }
         }
